b63cd22e8bc35d7a74170e20b329c24b82400567
Thibaut <thibautpierron@live.fr>
2017-08-02 13:32:33 +0200

    shadow and print board work

--- src/client/components/shadow.js
+++ src/client/components/shadow.js
@@ -19,9 +19,9 @@ function findHighestCell(e) {
 }
 
 function getShadow(tab, color) {
-    for (let i = 9; i >= 0; i--) {
+    for (let i = 11; i >= 0; i--) {
         let flag = false;
-        for (let j = 190 + i; j >= 0; j -= 10) {
+        for (let j = 240 + i; j >= 0; j -= 12) {
             if (tab[j] !== 0 || flag === true) {
                 tab[j] = color
                 flag = true
@@ -35,12 +35,12 @@ const Shadow = ({board}) => {
     const color = 4
     let shadow = getShadow([...board], color)
     shadow.forEach((e, i) => {
-    if (i % 12 === 0 || i % 12 === 11 || i < 12)
+    if (i % 12 === 0 || i % 12 === 11 || i > 240)
         shadow[i] = 8
     })
     shadow.reverse()
     const cells = [];
-    for (let i = 12; i < 252; i++) {
+    for (let i = 0; i < 240; i++) {
         cells.push(
             <Cell
                 key={i}
