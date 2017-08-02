a36abe4083be8b781f29da61c1a0c624ce6ea91f
thibautpierron <tpierron@student.42.fr>
2017-08-02 17:31:51 +0200

    add getter setter scores

--- src/server/player.js
+++ src/server/player.js
@@ -11,14 +11,20 @@ export default class Player {
     this._board = []
   }
 
-  toString ()       { return this._name }
-
   get position ()   { return this._position}
   get socketId ()   { return this._socketId }
   get board ()      { return this._board}
+  get name ()       { return this._name}
+  get level ()      { return this._level}
+  get lines ()      { return this._lines}
+  get score ()      { return this._score}
 
-  set name (name)   { this._name = name }
-  set board (tetro) { this._board.concat(tetro)}
+  set name (name)         { this._name = name }
+  set lines (lines)       { this._lines = lines }
+  set score (score)       { this._score = score }
+  set level (level)       { this._level = level }
+  set position (position) { this._position = position }
+  set board (tetro)       { this._board.concat(tetro)}
 
   incrementPosition () { this._position++ }
 }
