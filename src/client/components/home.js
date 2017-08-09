b8048bccbc59652f2020c8d6ac0a2f298729a5fd
Thibaut <thibautpierron@live.fr>
2017-08-09 09:55:19 +0200

    home done

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const Home = () => {
-
     return (
         <div className={"home"}>
             <div className={"homeImage"}></div>
             <div className={"homeMenu"}>
+                <h1></h1>
+                <div className={"homeButtonContainer"}>
+                    <div className={"homeButton"}>
+                        <div className={"cursor"}></div>
+                        <a href="#"><h1> create game</h1></a>
+                    </div>
+                    <div className={"homeButton"}>
+                        <div className={"cursor"}></div>
+                        <a href="#"><h1>join game</h1></a>
+                    </div>
+                </div>
                 <h1 className={"copyright"}>&copy;2017 amineau tpierron</h1>
             </div>
         </div>
