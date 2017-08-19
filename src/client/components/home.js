b444f8dc6c1eac47dcbb40ae0e38f90ec28bad43
Thibaut <thibautpierron@live.fr>
2017-08-19 07:44:57 +0200

    home player input done

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -4,22 +4,25 @@ import { Link } from 'react-router-dom'
 class Home extends React.Component {
     constructor(props) {
         super(props)
-        // console.log(this.props.players)
-        this.state = {name: "bob",
+        this.state = {name: "playr",
                     playerNameChecked: false}
         this.handleNameChange = this.handleNameChange.bind(this)
+        this.handleNameSubmit = this.handleNameSubmit.bind(this)
     }
 
     handleNameChange(event) {
-        const cmp = this.props.players.find((e) => {
-            return event.target.value === e
-        })
         this.setState({name: event.target.value})
-        if (cmp === undefined)
+    }
+
+    handleNameSubmit(event) {
+        const cmp = this.props.players.find((e) => {
+            return this.state.name === e
+        })
+        if (cmp === undefined && this.state.name !== "playr" && this.state.name !== "")
             this.setState({playerNameChecked: true})
         else
             this.setState({playerNameChecked: false})
-
+        event.preventDefault()      
     }
 
     render() {
@@ -45,15 +48,19 @@ class Home extends React.Component {
                         </div>
                     </div>
         const buttons = this.state.playerNameChecked ? enabledButtons : disabledButtons
+        let inputClass = "playerNameInput"
+        !this.state.playerNameChecked ? inputClass += " disabledButton" : 0
 
         return (
             <div className={"home"}>
                 <div className={"homeImage"}></div>
                 <div className={"homeMenu"}>
-                    <label style={{ fontSize: "30px" }}>
-                        Player name:
-                        <input type="text" value={this.state.name} onChange={this.handleNameChange} maxLength="5"/>
-                    </label>
+                    <form onSubmit={this.handleNameSubmit}>
+                        <label style={{ fontSize: "30px" }}>
+                            Player name:
+                            <input className={inputClass} type="text" value={this.state.name} onChange={this.handleNameChange} maxLength="5"/>
+                        </label>
+                    </form>
                     { buttons }
                     <h1 className={"copyright"}>&copy;2017 amineau tpierron</h1>
                 </div>
