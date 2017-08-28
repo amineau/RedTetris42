6698033317f470c08d968b64962f5da9e3a7dac4
amineau <amineau@student.42.fr>
2017-08-28 16:21:15 +0200

    MAJ create game's view

--- src/client/components/create.js
+++ src/client/components/create.js
@@ -9,62 +9,48 @@ class Create extends React.Component {
             roomNameChecked: true
         }
         this.handleNameChange = this.handleNameChange.bind(this)
-        this.handleNameSubmit = this.handleNameSubmit.bind(this)
     }
 
     handleNameChange(event) {
+        if (this.comparePlayersName(event.target.value))
+            this.setState({roomNameChecked: true})
+        else
+            this.setState({roomNameChecked: false})
         this.setState({name: event.target.value})
     }
 
-    handleNameSubmit(event) {
-        event.preventDefault()        
+    comparePlayersName(name) {
         const cmp = this.props.list.room.find((e) => {
-            return this.state.name === e.name
+            return name === e
         })
-        if (cmp === undefined && this.state.name !== ""){
-            this.setState({roomNameChecked: true})
-        } else {
-            this.setState({roomNameChecked: false})
-        }
+        console.log({nameRoomCompare:!cmp && name !== "playr" && name !== ""})
+        return !cmp && name !== ""
+
     }
 
     render() {
-        const enabledButtons = 
-                    <div className={"homeButtonContainer"}>
-                        <div className={"homeButton"}>
-                            <div className={"cursor"}></div>
-                            <Link to='/'><h1>cancel</h1></Link>
-                        </div>
-                        <div className={"homeButton"}>
-                            <div className={"cursor"}></div>
-                            <Link to={`/game/${this.state.name}[${this.props.playerName}]`}><h1>ok</h1></Link>
-                        </div>
-                    </div>
-
-        const disabledButtons = 
-                    <div className={"homeButtonContainer"}>
-                        <div className={"homeButton"}>
-                            <div className={"cursor"}></div>
-                            <Link to='/'><h1>cancel</h1></Link>
-                        </div>
-                        <div className={"homeButton"}>
-                            <h1 className={"disabledButton"}>ok</h1>
-                        </div>
-                    </div>
-        const buttons = this.state.roomNameChecked ? enabledButtons : disabledButtons
+        const buttonClass = this.state.roomNameChecked ? "" : "disabledLink"
+        const titleClass = this.state.roomNameChecked ? "" : "disabledButton"
         let inputClass = "roomNameInput"
         !this.state.roomNameChecked ? inputClass += " disabledButton" : 0
 
         return (
             <div className={"home"}>
                 <div className={"homeMenu"}>
-                    <form onSubmit={this.handleNameSubmit}>
+                    <form  onSubmit={(e) => {e.preventDefault()}}>
                         <label style={{ fontSize: "30px" }}>
                             Room name:
                             <input className={inputClass} type="text" value={this.state.name} onChange={this.handleNameChange} maxLength="50"/>
                         </label>
                     </form>
-                    { buttons }
+                     <div className={"homeButtonContainer"}>
+                        <div className={"homeButton"}>
+                            <div className={"cursor"}></div>
+                            <Link to={`/game/${this.state.name}[${this.props.playerName}]`} className={buttonClass}>
+                                <h1 className={titleClass}>ok</h1>
+                            </Link>
+                        </div>
+                    </div>
                     <h1 className={"copyright"}>&copy;2017 amineau tpierron</h1>
                 </div>
             </div>
